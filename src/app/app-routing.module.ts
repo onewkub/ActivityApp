7d69c393df0d3e31e3e9ea9749bc3e445cd3f42b
Onewkub <oldnew123@gmail.com>
2019-11-17 18:48:32 +0700

    add login and register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from "./components/homepage/homepage.component";
 import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
 
 const routes: Routes = [
   { path: '', component: HomepageComponent, children: [
     { path: '', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
   ]},
 ];
 
