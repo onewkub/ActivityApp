5855bd1105dee297afeee4261274b64b975d217c
Onewkub <oldnew123@gmail.com>
2019-11-21 19:45:31 +0700

    Login Register It's Okay

--- src/app/components/mainpage/mainpage.component.ts
+++ src/app/components/mainpage/mainpage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mainpage',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService : AuthService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
   }
-
+  onLogout(){
+    console.log("logout");
+    this.authService.tryLogout()
+    .then(res=>{
+      console.log(res);
+      this.router.navigate(['/']);
+    });
+  }
 }
